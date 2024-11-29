9db44cd81a8833e00e22f23f64a02d6f07e908f3
Harshith-UI <harshikapuluru@gmail.com>
2024-11-29 12:51:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,17 @@
+import Amplify from 'aws-amplify';
 
-const awsConfig = {
-    region: 'us-east-1',
-    userPoolId: '<Your-User-Pool-ID>',
-    userPoolWebClientId: '<Your-App-Client-ID>',
-};
-Amplify.configure({ Auth: awsConfig });
+Amplify.configure({
+    Auth: {
+        region: 'us-east-1',
+        userPoolId: 'us-east-1_OPmUKqybN',
+        userPoolWebClientId: '62rbt4bbiai2ifpir22rh8jh1g',
+        mandatorySignIn: true,
+        oauth: {
+            domain: 'chatapp.auth.us-east-1.amazoncognito.com',
+            scope: ['email', 'openid', 'profile'],
+            redirectSignIn: 'https://main.d1uarapbmol90y.amplifyapp.com',
+            redirectSignOut: 'https://main.d1uarapbmol90y.amplifyapp.com',
+            responseType: 'code'
+        }
+    }
+});
