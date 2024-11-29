0cf5f2a0c57c7a39e4a3875e2321f3fd2b9e71cf
Harshith-UI <harshikapuluru@gmail.com>
2024-11-29 12:19:06 +0530

    Delete public_project directory

--- public_project/config.js
+++
@@ -1,7 +0,0 @@
-
-const awsConfig = {
-    region: 'us-east-1',
-    userPoolId: '<Your-User-Pool-ID>',
-    userPoolWebClientId: '<Your-App-Client-ID>',
-};
-Amplify.configure({ Auth: awsConfig });
