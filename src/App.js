2c7f7241e9a4a4168beb965c4dba203a986a69c6
Harshith-UI <harshikapuluru@gmail.com>
2024-11-29 11:31:51 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import ChatRoom from './components/ChatRoom';
+import Login from './components/Login';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/room/:roomId" element={<ChatRoom />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
