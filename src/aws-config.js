05ddfb380b528dac4b8f7097ef1827aacab29ac3
Harshith-UI <harshikapuluru@gmail.com>
2024-11-29 11:36:33 +0530

    Delete src directory

--- src/aws-config.js
+++
@@ -1,9 +0,0 @@
-import { Amplify } from 'aws-amplify';
-
-Amplify.configure({
-  Auth: {
-    region: 'us-east-1',
-    userPoolId: 'us-east-1_OPmUKaybN',
-    userPoolWebClientId: '62rbt4bbiai2ifpir22rh8jh1g',
-  },
-});
